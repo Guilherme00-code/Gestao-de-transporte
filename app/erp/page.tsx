f4c4ce184a864328c3611ec4a43a114b76965418
Guilherme00-code <guilherme2008.fl@gmail.com>
2026-09-10 08:45:18 -0300

    restore ERP navigation route

--- app/erp/page.tsx
+++ app/erp/page.tsx
@@ -1,5 +1,22 @@
+import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import ErpModules from '@/components/erp-modules'
+import AdvancedModules from '@/components/advanced-modules'
+import { getErpData } from '@/app/actions/erp'
+import { getAdvancedData } from '@/app/actions/advanced'
+import { getManagementData } from '@/app/actions/management'
+import { auth } from '@/lib/auth'
 
-export default function ErpPage() {
-  redirect('/')
+export const dynamic = 'force-dynamic'
+
+export default async function ErpPage() {
+  const session = await auth.api.getSession({ headers: await headers() })
+  if (!session?.user) redirect('/sign-in')
+  const [management, data, advanced] = await Promise.all([getManagementData(), getErpData(), getAdvancedData()])
+  return (
+    <>
+      <ErpModules fleet={management.fleet} team={management.team} data={data} />
+      <AdvancedModules fleet={management.fleet} team={management.team} data={advanced} />
+    </>
+  )
 }
